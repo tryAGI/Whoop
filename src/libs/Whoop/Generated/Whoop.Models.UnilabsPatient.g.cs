82c169ba840f7dc5075275e76d56cdc06ba5d224
Konstantin S. <havendv@gmail.com>
2026-04-16 23:10:02 +0400

    feat: Updated OpenAPI spec (#8)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Whoop/Generated/Whoop.Models.UnilabsPatient.g.cs
+++ src/libs/Whoop/Generated/Whoop.Models.UnilabsPatient.g.cs
@@ -30,11 +30,47 @@ namespace Whoop
         public required global::System.DateTime BirthDate { get; set; }
 
         /// <summary>
+        /// Gender of the patient
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("gender")]
+        public string? Gender { get; set; }
+
+        /// <summary>
+        /// Email address of the patient
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("email")]
+        public string? Email { get; set; }
+
+        /// <summary>
+        /// Phone number of the patient
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("phone")]
+        public string? Phone { get; set; }
+
+        /// <summary>
+        /// Nationality of the patient
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("nationality")]
+        public string? Nationality { get; set; }
+
+        /// <summary>
         /// Unique identifier for the patient
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("id")]
         [global::System.Text.Json.Serialization.JsonRequired]
         public required global::System.Guid Id { get; set; }
+
+        /// <summary>
+        /// Emirates ID of the patient
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("emirates_id")]
+        public string? EmiratesId { get; set; }
+
+        /// <summary>
+        /// Passport number of the patient
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("passport_no")]
+        public string? PassportNo { get; set; }
 
         /// <summary>
         /// Additional properties that are not explicitly defined in the schema
@@ -57,6 +93,24 @@ namespace Whoop
         /// <param name="id">
         /// Unique identifier for the patient
         /// </param>
+        /// <param name="gender">
+        /// Gender of the patient
+        /// </param>
+        /// <param name="email">
+        /// Email address of the patient
+        /// </param>
+        /// <param name="phone">
+        /// Phone number of the patient
+        /// </param>
+        /// <param name="nationality">
+        /// Nationality of the patient
+        /// </param>
+        /// <param name="emiratesId">
+        /// Emirates ID of the patient
+        /// </param>
+        /// <param name="passportNo">
+        /// Passport number of the patient
+        /// </param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
@@ -64,12 +118,24 @@ namespace Whoop
             string firstName,
             string lastName,
             global::System.DateTime birthDate,
-            global::System.Guid id)
+            global::System.Guid id,
+            string? gender,
+            string? email,
+            string? phone,
+            string? nationality,
+            string? emiratesId,
+            string? passportNo)
         {
             this.FirstName = firstName ?? throw new global::System.ArgumentNullException(nameof(firstName));
             this.LastName = lastName ?? throw new global::System.ArgumentNullException(nameof(lastName));
             this.BirthDate = birthDate;
+            this.Gender = gender;
+            this.Email = email;
+            this.Phone = phone;
+            this.Nationality = nationality;
             this.Id = id;
+            this.EmiratesId = emiratesId;
+            this.PassportNo = passportNo;
         }
 
         /// <summary>
