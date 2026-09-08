a375cc2be7d0a0d4ff019a9528745a0ecc9330bb
Konstantin S. <havendv@gmail.com>
2026-09-08 17:20:23 +0400

    feat: Updated OpenAPI spec (#221)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Whoop/Generated/Whoop.Models.Appointment.g.cs
+++ src/libs/Whoop/Generated/Whoop.Models.Appointment.g.cs
@@ -9,18 +9,18 @@ namespace Whoop
     public sealed partial class Appointment
     {
         /// <summary>
-        /// The service request IDs associated with this appointment
-        /// </summary>
-        [global::System.Text.Json.Serialization.JsonPropertyName("service_request_ids")]
-        [global::System.Text.Json.Serialization.JsonRequired]
-        public required global::System.Collections.Generic.IList<global::System.Guid> ServiceRequestIds { get; set; }
-
-        /// <summary>
         /// The start time of the appointment
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("start_time")]
         [global::System.Text.Json.Serialization.JsonRequired]
         public required global::System.DateTime StartTime { get; set; }
+
+        /// <summary>
+        /// The service request IDs associated with this appointment
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("service_request_ids")]
+        [global::System.Text.Json.Serialization.JsonRequired]
+        public required global::System.Collections.Generic.IList<global::System.Guid> ServiceRequestIds { get; set; }
 
         /// <summary>
         ///
@@ -38,23 +38,23 @@ namespace Whoop
         /// <summary>
         /// Initializes a new instance of the <see cref="Appointment" /> class.
         /// </summary>
-        /// <param name="serviceRequestIds">
-        /// The service request IDs associated with this appointment
-        /// </param>
         /// <param name="startTime">
         /// The start time of the appointment
+        /// </param>
+        /// <param name="serviceRequestIds">
+        /// The service request IDs associated with this appointment
         /// </param>
         /// <param name="type"></param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public Appointment(
-            global::System.Collections.Generic.IList<global::System.Guid> serviceRequestIds,
             global::System.DateTime startTime,
+            global::System.Collections.Generic.IList<global::System.Guid> serviceRequestIds,
             string type)
         {
-            this.ServiceRequestIds = serviceRequestIds ?? throw new global::System.ArgumentNullException(nameof(serviceRequestIds));
             this.StartTime = startTime;
+            this.ServiceRequestIds = serviceRequestIds ?? throw new global::System.ArgumentNullException(nameof(serviceRequestIds));
             this.Type = type ?? throw new global::System.ArgumentNullException(nameof(type));
         }
 
